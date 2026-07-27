7dc96dbfeb6df002c97f2bb2c651bab11ff4dc58
ktheagent <110701561+ktheagent@users.noreply.github.com>
2026-07-27 02:21:01 +0000

    Restore Build 29 core module music theory

---
+++ src/core/music-theory.js
@@ -0,0 +1,298 @@
+‚€ЫЫњЭUТРУTФСTЧФТT”HЙРЙЛ	РИЙЛ	С	Л	СЙЛ	СIЛ	С‰Л	С€ЙЛ	СЙЛ	СИЙЛ	РIЛ	РHЙЛ	Р‰ЧNВ€ЫЫњЭUТРУTФСTЧС“UHЙРЙЛ	С‰Л	С	Л	СX‰Л	СIЛ	С‰Л	СШ‰Л	СЙЛ	РX‰Л	РIЛ	Р‰Л	Р‰ЧNВ€ЫЫњЭUT”ИHЙРЙЛ	С	Л	СIЛ	С‰Л	СЙЛ	РIЛ	Р‰ЧNВ€ЫЫњЭUT—ХЧФИHИО€€‹N€Ћ€KО€ЛN€KЋ€LHNВ€ЫЫњЭPR“Ф—ФРРSHHМ‹KЛKLWNВ€ЫЫњЭђUTђSУRS“Ф—ФРРSHHМ‹ЛKЛLNВ€ЫЫњЭТT”УФ‘T€HЙС‰Л	РЙЛ	СЙЛ	С	Л	РIЛ	СIЛ	Р‰ЧNВ€ЫЫњЭ“UУФ‘T€HЙР‰Л	СIЛ	РIЛ	С	Л	СЙЛ	РЙЛ	С‰ЧNВ€ЫЫњЭСVWФТQУђUT‘TИHВ€О€О€K€‹N€ЛN€Ћ€K	С€ЙО€‹	РИЙО€Л€Ћ€LKЋ€L‹XЋ€LЛXЋ€MЋ€MKШЋ€M‹ШЋ€MЛ€[N€[N€K›N€‹	С€ЫIО€Л	РИЫIО€	СИЫIО€K	СЫIО€‹	РHЫIО€Л€N€LKЫN€L‹ЫN€LЛ›N€M›N€MKX›N€M‹X›N€MВ€NВ‚€ЛИX]ЫљXИ[™^Щ€H]ЪЫ€XXЪЫY‰ЬИ›ЭЫH[™K‚€ЫЫњЭУQ—Р“ХУWУS‘TИHВ€™X›N€МЛИM€	Э™X›KN	О€МЛИЬљ][€MИЫЭ[™ИЫ™HШЭ]™HЭЩ\‚€\ЬО€NЛИМ‚€[О€ЌЛИЊВ€[›ЬЋ€Њ€ЛИВ€NВ‚€ќ[Э[Ы€Ы[\
+[YKZ[‹X^
+HИ™]\›€X]›X^
+Z[‹X]›Z[ЉX^[YJJNИB€ќ[Э[Ы€[Щ
+[YK]љ\ЫЬЉHИ™]\›€
+
+[YH	H]љ\ЫЬЉH
+И]љ\ЫЬЉH	H]љ\ЫЬЋИB‚€ќ[Э[Ы€\њЩT]Ъ
+]Ъ
+HВ€ЫЫњЭX]ЪHЧЉРKQШKYЧJJИШ—^МџJJOЧ
+КIЛ™^XКЭљ[™К]Ъ	ЙКKќљ[J
+JNВ€Y€
+[X]Ъ
+H›ЭИ™]И\њ›ЬЉ[ќ[Y]Ъ€	Ь]ЪX
+NВ€ЫЫњЭ]\€HX]ЪМWKќХ\\ђШ\ЩJ
+NВ€ЫЫњЭXШЪY[ќ[^HX]ЪМ—NВ€ЫЫњЭXШЪY[ќ[HЛ‹‹XШЪY[ќ[^Kњ™YXЩJ
+Э[KЪ\ЉHO€Э[H
+И
+Ъ\€OOH	ИЙИИH€LJK
+NВ€™]\›€И]\‹XШЪY[ќ[XШЪY[ќ[^ШЭ]™N€ќ[X™\ЉX]ЪМЧJHNВ€B‚€ќ[Э[Ы€XШЪY[ќ[^
+[\][ЫЉHВ€ЫЫњЭ[YHHX]њ›Э[™
+ќ[X™\Љ[\][ЫЉH
+NВ€Y€
+[YH€
+H™]\›€	ИЙЛњ™\X]
+[YJNВ€Y€
+[YH
+H™]\›€	Ш‰Лњ™\X]
+][YJNВ€™]\›€	ЙОВ€B‚€ќ[Э[Ы€]ЪУZYJ]Ъ
+HВ€Y€
+\[Щ€]ЪOOH	Ыќ[X™\‰КH™]\›€Ы[\
+X]њ›Э[™
+]Ъ
+KLЌКNВ€ЫЫњЭ\њЩYH\њЩT]Ъ
+]Ъ
+NВ€™]\›€Ы[\
+
+\њЩY›ШЭ]™H
+ИJH
+€L€
+ИUT—ХЧФЦЬ\њЩY›]\—H
+И\њЩYXШЪY[ќ[LЌКNВ€B‚€ќ[Э[Ы€ZYUФ]Ъ
+ZYK™Y™\‘›]ИH[ЩJHВ€ЫЫњЭ[YHHЫ[\
+X]њ›Э[™
+ќ[X™\ЉZYJJKLЌКNВ€ЫЫњЭ[Y\ИH™Y™\‘›]ИИUТРУTФСTЧС“U€UТРУTФСTЧФТT”В€™]\›€	Ы[Y\ЦЭ[YH	HL—_IУX]™›ЫЬЉ[YHИLЉHH_XВ€B‚€ќ[Э[Ы€Щ^T›ЫЭ
+Щ^HH	РЙКHВ€ЫЫњЭЫX[€HЭљ[™КЩ^JKњ™\XЩJЫIЛ	ЙКNВ€ЫЫњЭX]ЪHЧЉРKQЧJJИШ—OКIЛ™^XКЫX[ЉNВ€Y€
+[X]Ъ
+H™]\›€И]\Ћ€	РЙЛXШЪY[ќ[€О€^€	РЙИNВ€ЫЫњЭXШЪY[ќ[HX]ЪМ—HOOH	ИЙИИH€X]ЪМ—HOOH	Ш‰ИИLH€В€™]\›€И]\Ћ€X]ЪМWKXШЪY[ќ[О€[Щ
+UT—ХЧФЦЫX]ЪМWWH
+ИXШЪY[ќ[LЉK^€ЫX[€NВ€B‚€ќ[Э[Ы€Щ^T›ЫЭКЩ^HH	РЙКHИ™]\›€Щ^T›ЫЭ
+Щ^JKњОИB€ќ[Э[Ы€\УZ[›Ь’Щ^JЩ^HH	РЙЛ[ЩJHИ™]\›€[ЩHИ[ЩHOOH	ЫZ[›Ь‰И€ЫIЛќ\Э
+Эљ[™КЩ^JJNИB‚€ќ[Э[Ы€Щ^TЪYЫ]\™PЫЭ[ќ
+Щ^HH	РЙКHВ€ЫЫњЭ[YHHСVWФТQУђUT‘TЦФЭљ[™КЩ^JWNВ€Y€
+ќ[X™\‹љ\Т[ќYЩ\Љ[YJJH™]\›€[YNВ€™]\›€СVWФТQУђUT‘TЦФЭљ[™КЩ^JKњ™\XЩJЫIЛ	ЙКWHВ€B‚€ќ[Э[Ы€Щ^TЪYЫ]\™P[\][ЫЉ]\‹Щ^HH	РЙКHВ€ЫЫњЭЫЭ[ќHЩ^TЪYЫ]\™PЫЭ[ќ
+Щ^JNВ€Y€
+ЫЭ[ќ€	‰€ТT”УФ‘T‹њЫXЩJЫЭ[ќ
+Kљ[ЫY\К]\ЉJH™]\›€NВ€Y€
+ЫЭ[ќ	‰€“UУФ‘T‹њЫXЩJXЫЭ[ќ
+Kљ[ЫY\К]\ЉJH™]\›€LNВ€™]\›€В€B‚€ќ[Э[Ы€ШШ[Q›Ь’Щ^JЩ^HH	РЙЛ[ЩJHВ€ЫЫњЭ›Ь›][HH\УZ[›Ь’Щ^JЩ^K[ЩJHИђUTђSУRS“Ф—ФРРSH€PR“Ф—ФРРSNВ€ЫЫњЭ›ЫЭHЩ^T›ЫЭКЩ^JNВ€™]\›€›Ь›][K›X\
+Э\O€[Щ
+›ЫЭ
+ИЭ\LЉJNВ€B‚€ќ[Э[Ы€]ЪСX]ЫљXТ[™^
+]Ъ
+HВ€ЫЫњЭИ]\‹ШЭ]™HHH\њЩT]Ъ
+]Ъ
+NВ€™]\›€ШЭ]™H
+€И
+ИUT”Лљ[™^ЩЉ]\ЉNВ€B‚€ќ[Э[Ы€X]ЫљXТ[™^У]\[
+[™^
+HВ€ЫЫњЭ›Э[™YHX]њ›Э[™
+ќ[X™\Љ[™^
+JNВ€™]\›€И]\Ћ€UT”ЦЫ[Щ
+›Э[™YКWKШЭ]™N€X]™›ЫЬЉ›Э[™YИКHNВ€B‚€ќ[Э[Ы€]Ъњ›ЫQX]ЫљXТ[™^
+[™^Щ^HH	РЙЛ^XЪ][\][Ы€Hќ[
+HВ€ЫЫњЭИ]\‹ШЭ]™HHHX]ЫљXТ[™^У]\[
+[™^
+NВ€ЫЫњЭ[\][Ы€H^XЪ][\][Ы€OHќ[ИЩ^TЪYЫ]\™P[\][ЫЉ]\‹Щ^JH€ќ[X™\Љ^XЪ][\][ЫЉNВ€™]\›€	Ы]\џIШXШЪY[ќ[^
+[\][ЫЉ_IЫШЭ]™_XВ€B‚€ќ[Э[Ы€Ь[ZYQ›Ь’Щ^JZYKЩ^HH	РЙЛ™Y™\њ™Y]\€Hќ[
+HВ€ЫЫњЭ[YHHЫ[\
+X]њ›Э[™
+ќ[X™\ЉZYJJKLЌКNВ€ЫЫњЭЩ^PЫЭ[ќHЩ^TЪYЫ]\™PЫЭ[ќ
+Щ^JNВ€ЫЫњЭШ[™Y]\ИHЧNВ€›Ь€
+ЫЫњЭ]\€Щ€UT”КHВ€Y€
+™Y™\њ™Y]\€	‰€]\€OOH™Y™\њ™Y]\ЉHЫЫќ[ќYNВ€ЫЫњЭ\›Ю[X]SШЭ]™HHX]™›ЫЬЉ[YHИLЉHHNВ€›Ь€
+]ШЭ]™HH\›Ю[X]SШЭ]™HHNИШЭ]™HH\›Ю[X]SШЭ]™H
+ИNИШЭ]™H
+ПHJHВ€ЫЫњЭ]\[ZYHH
+ШЭ]™H
+ИJH
+€L€
+ИUT—ХЧФЦЫ]\—NВ€ЫЫњЭ[\][Ы€H[YHH]\[ZYNВ€Y€
+[\][Ы€L€[\][Ы€€ЉHЫЫќ[ќYNВ€ЫЫњЭ^XЭYHЩ^TЪYЫ]\™P[\][ЫЉ]\‹Щ^JNВ€ЫЫњЭЬљY[ќ][Ы”[[HHЩ^PЫЭ[ќ€	‰€[\][Ы€И€Щ^PЫЭ[ќ	‰€[\][Ы€€И€В€ЫЫњЭШЫЬ™HHX]XњК[\][Ы€H^XЭY
+H
+€
+ИX]XњК[\][ЫЉH
+€€
+ИЬљY[ќ][Ы”[[NВ€Ш[™Y]\Лњ\Ъ
+ИШЫЬ™K]Ъ€	Ы]\џIШXШЪY[ќ[^
+[\][ЫЉ_IЫШЭ]™_XJNВ€B€B€Y€
+XШ[™Y]\Л›[™Э
+H™]\›€ZYUФ]Ъ
+[YKЩ^PЫЭ[ќ
+NВ€Ш[™Y]\ЛњЫЬќ
+
+KЉHO€KњШЫЬ™HH‹њШЫЬ™HKњ]Ъ›ШШ[PЫЫ\\™J‹њ]Ъ
+JNВ€™]\›€Ш[™Y]\ЦМKњ]ЪВ€B‚€ќ[Э[Ы€[њЬЬЩRЩ^JЩ^HH	РЙЛЩ[Z]Ы™\ИH
+HВ€ЫЫњЭZ[›Ь€HЫIЛќ\Э
+Эљ[™КЩ^JJNВ€ЫЫњЭ\™Щ]ИH[Щ
+Щ^T›ЫЭКЩ^JH
+Иќ[X™\ЉЩ[Z]Ы™\КKLЉNВ€ЫЫњЭ™Y™\‘›]ИHЩ^TЪYЫ]\™PЫЭ[ќ
+Щ^JHВ€ЫЫњЭ[YHH
+™Y™\‘›]ИИUТРУTФСTЧС“U€UТРУTФСTЧФТT”
+VЭ\™Щ]ЧNВ€™]\›€	Ы[Y_IЫZ[›Ь€И	ЫIИ€	ЙЯXВ€B‚€ќ[Э[Ы€ЫYђ›ЭЫS[™R[™^
+ЫY€H	Э™X›IКHИ™]\›€УQ—Р“ХУWУS‘TЦШЫY—HПИУQ—Р“ХУWУS‘TЛќ™X›NИB€ќ[Э[Ы€ЭY™”Э\›Ь”]Ъ
+]ЪЫY€H	Э™X›IКHИ™]\›€]ЪСX]ЫљXТ[™^
+]Ъ
+HHЫYђ›ЭЫS[™R[™^
+ЫYЉNИB€ќ[Э[Ы€]Ъ›Ь”ЭY™”Э\
+Э\ЫY€H	Э™X›IЛЩ^HH	РЙКHИ™]\›€]Ъњ›ЫQX]ЫљXТ[™^
+ЫYђ›ЭЫS[™R[™^
+ЫYЉH
+ИX]њ›Э[™
+Э\
+KЩ^JNИB‚€ќ[Э[Ы€\Ь^SZYQ›Ь”\ќ
+ZYK\ќHЯKЫЫЩ\ќ]ЪHќYJHВ€ЛИШЭ]™K][њЬЬЪ[™ИЫYњИY™™XЭHЬљ][€ЭY™€[€›ЭЫЫЩ\ќH[™€ЛИ[њЬЬЩY\]ЪљY]ЬЛ€[њЭќ[Y[ќ[њЬЬЪ][Ы€\ИY[€[€ЫЫЩ\ќ]Ъ‚€ЫЫњЭЫY“ШЭ]™TЪYќH\ќЫY€OOH	Э™X›KN	ИИL€€В€ЫЫњЭ[њЭќ[Y[ќЪYќHЫЫЩ\ќ]ЪИ€ќ[X™\Љ\ќќ[њЬЬЩH
+NВ€™]\›€Ы[\
+ќ[X™\ЉZYJH
+ИЫY“ШЭ]™TЪYќ
+И[њЭќ[Y[ќЪYќLЌКNВ€B‚€ќ[Э[Ы€ЫЭ[™[™УZYQњ›ЫQ\Ь^J\Ь^SZYK\ќHЯKЫЫЩ\ќ]ЪHќYJHВ€ЫЫњЭЫY“ШЭ]™TЪYќH\ќЫY€OOH	Э™X›KN	ИИL€€В€ЫЫњЭ[њЭќ[Y[ќЪYќHЫЫЩ\ќ]ЪИ€ќ[X™\Љ\ќќ[њЬЬЩH
+NВ€™]\›€Ы[\
+ќ[X™\Љ\Ь^SZYJHHЫY“ШЭ]™TЪYќH[њЭќ[Y[ќЪYќLЌКNВ€B‚€ќ[Э[Ы€Ьљ][”]Ъ›Ь‘]™[ќ
+]™[ќ\ќHЯKЩ^HH	РЙЛЫЫЩ\ќ]ЪHќYJHВ€ЫЫњЭ\Ь^SZYHH\Ь^SZYQ›Ь”\ќ
+]™[ќ›ZYK\ќЫЫЩ\ќ]Ъ
+NВ€Y€
+]™[ќќЬљ][”]Ъ	‰€XЫЫЩ\ќ]Ъ
+HВ€ћHИY€
+]ЪУZYJ]™[ќќЬљ][”]Ъ
+HOOH\Ь^SZYJH™]\›€]™[ќќЬљ][”]ЪИHШ]Ъ
+КHЯB€B€Y€
+]™[ќњ]Ъ
+HВ€ћHИY€
+]ЪУZYJ]™[ќњ]Ъ
+HOOH\Ь^SZYJH™]\›€]™[ќњ]ЪИHШ]Ъ
+КHЯB€B€™]\›€Ь[ZYQ›Ь’Щ^J\Ь^SZYKЩ^JNВ€B‚€ќ[Э[Ы€YЬ™YQ›Ь”]Ъ
+]ЪЩ^HH	РЙКHВ€ЫЫњЭ\њЩYH\њЩT]Ъ
+]Ъ
+NВ€ЫЫњЭЫљXИHЩ^T›ЫЭ
+Щ^JNВ€ЫЫњЭYЬ™YHH[Щ
+UT”Лљ[™^ЩЉ\њЩY›]\ЉHHUT”Лљ[™^ЩЉЫљXЛ›]\ЉKКH
+ИNВ€ЛИЛX\ЩYZ[›ЬЋ€HЭЩ\™YYЬ™Y\И\™H™\™\Щ[ќYћHЪ›ЫX]XИЮ[X›\И
+YKKJK‚€ЫЫњЭ^XЭYЩ™њЩ]HPR“Ф—ФРРSVЩYЬ™YHHWNВ€ЫЫњЭXЭX[ИH[Щ
+UT—ХЧФЦЬ\њЩY›]\—H
+И\њЩYXШЪY[ќ[LЉNВ€ЫЫњЭXЭX[Щ™њЩ]H[Щ
+XЭX[ИHЫљXЛњЛLЉNВ€][\][Ы€HXЭX[Щ™њЩ]H^XЭYЩ™њЩ]В€Y€
+[\][Ы€€ЉH[\][Ы€OHLЋВ€Y€
+[\][Ы€MЉH[\][Ы€
+ПHLЋВ€™]\›€ИYЬ™YKXШЪY[ќ[€[\][Ы€NВ€B‚€ќ[Э[Ы€YЬ™YQ›Ь“ZYJZYKЩ^HH	РЙКHИ™]\›€YЬ™YQ›Ь”]Ъ
+Ь[ZYQ›Ь’Щ^JZYKЩ^JKЩ^JNИB‚€ќ[Э[Ы€ЫљXФ™Y™\™[ЩSZYJЩ^HH	РЙКHВ€ЫЫњЭЫљXИHЩ^T›ЫЭ
+Щ^JNВ€™]\›€]ЪУZYJ	ЭЫљXЛ›]\џIШXШЪY[ќ[^
+ЫљXЛXШЪY[ќ[
+_M
+NВ€B‚€ЛИЭ\ќЩ[‹XЫЫ\]X›H[ЭX›KYИЪ›ЫX]XИЮ[X›\Л€›ИИЬ€€Ъ\XЭ\њИ\™H[Z]Y‚€ќ[Э[Ы€ЫљXФЫЫQ›Ь”]Ъ
+]ЪЩ^HH	РЙЛЬ[ЫњИHЯJHВ€ЫЫњЭЪЬќ\ЩHHЙЩ	Л	Ь‰Л	ЫIЛ	Щ‰Л	ЬЙЛ	Ы	Л	Э	ЧNВ€ЫЫњЭЫ™Р\ЩHHЙЩЙЛ	Ь™IЛ	ЫZIЛ	ЩIЛ	ЬЫЫ	Л	ЫIЛ	ЭIЧNВ€ЫЫњЭZ\ЩYHЙЩIЛ	ЬљIЛ	ЫXIЛ	ЩљIЛ	ЬЪIЛ	ЫIЛ	ЭIЧNВ€ЫЫњЭЭЩ\™YHЙЩIЛ	ЬIЛ	ЫYIЛ	Щ™IЛ	ЬЩIЛ	ЫIЛ	ЭIЧNВ€ЫЫњЭИYЬ™YKXШЪY[ќ[HHYЬ™YQ›Ь”]Ъ
+]ЪЩ^JNВ€]^В€Y€
+XШЪY[ќ[€
+H^HZ\ЩYЩYЬ™YHHWNВ€[ЩHY€
+XШЪY[ќ[
+H^HЭЩ\™YЩYЬ™YHHWNВ€[ЩH^H
+Ь[ЫњЛ›Ы™ИИЫ™Р\ЩH€ЪЬќ\ЩJVЩYЬ™YHHWNВ€ЛИ^™[Y[H\™HЭX›H[\][ЫњИ™[XZ[€Ю[X›ЫYњ™YHќ]љ\ЪX›H\Э[Э‚€Y€
+X]XњКXШЪY[ќ[
+H€JH^
+ПHXШЪY[ќ[€И	ЪIЛњ™\X]
+X]XњКXШЪY[ќ[
+HHJH€	ШIЛњ™\X]
+X]XњКXШЪY[ќ[
+HHJNВ‚€ЫЫњЭZYHH]ЪУZYJ]Ъ
+NВ€ЫЫњЭШЭ]™HHX]™›ЫЬЉ
+ZYHHЫљXФ™Y™\™[ЩSZYJЩ^JJHИLЉNВ€Y€
+ШЭ]™H€
+H^
+ПHЬ[ЫњЛ›Ы™ИИ	ш n‰Лњ™\X]
+X]›Z[ЉШЭ]™KКJH€‰И‹њ™\X]
+X]›Z[ЉШЭ]™KКJNВ€Y€
+ШЭ]™H
+H^
+ПHЬ[ЫњЛ›Ы™ИИ	ш ўЙЛњ™\X]
+X]›Z[Љ[ШЭ]™KКJH€	Л	Лњ™\X]
+X]›Z[Љ[ШЭ]™KКJNВ€™]\›€^В€B‚€ќ[Э[Ы€ЫљXФЫЫQ›Ь“ZYJZYKЩ^HH	РЙЛЬ[ЫњИHЯJHВ€™]\›€ЫљXФЫЫQ›Ь”]Ъ
+Ь[ЫњЛњ]ЪЬ[ZYQ›Ь’Щ^JZYKЩ^JKЩ^KЬ[ЫњКNВ€B‚€ќ[Э[Ы€[Y]T]ЪY[ќ]J]™[ќЩ^HH	РЙЛ\ќHЯKЫЫЩ\ќ]ЪHќYJHВ€Y€
+Y]™[ќ]™[ќќ\HOOH	Ы›ЭIКH™]\›€И[Y€ќYHNВ€ћHВ€ЫЫњЭ]ЪHЬљ][”]Ъ›Ь‘]™[ќ
+]™[ќ\ќЩ^KЫЫЩ\ќ]Ъ
+NВ€ЫЫњЭ\Ь^SZYHH\Ь^SZYQ›Ь”\ќ
+]™[ќ›ZYK\ќ, concertPitch);
+      return {
+        valid: pitchToMidi(pitch) === displayMidi,
+        pitch,
+        midi: Number(event.midi),
+        displayMidi,
+        solfa: tonicSolfaForPitch(pitch, key)
+      };
+    } catch (error) {
+      return { valid: false, pitch: null, midi: Number(event.midi), error: error.message };
+    }
+  }
+
+  function intervalSemitones(value) {
+    if (typeof value === 'number' && Number.isFinite(value)) return Math.round(value);
+    const raw = String(value ?? '').trim();
+    if (!raw) throw new Error('Enter an interval such as m3, M3, P5, P8, or a semitone number.');
+    if (/^[[+-]?\d+(?:\.0+)?$/.test(raw)) return Math.round(Number(raw));
+    const aliases = { octave: 'P8', unison: 'P1', tritone: 'A4' };
+    const token = aliases[raw.toLowerCase()] || raw;
+    const match = token.match(/^([PpMmAaDd])\s*(\d+)$/);
+    if (!match) throw new Error(`Unsupported interval: ${raw}`);
+    const qualityRaw = match[1];
+    const quality = qualityRaw === 'p' ? 'P' : qualityRaw;
+    const number = Number(match[2]);
+    if (!Number.isInteger(number) || number < 1 || number > 22) throw new Error(`Interval number is out of range: ${number}`);
+    const simple = ((number - 1) % 7) + 1;
+    const octaves = Math.floor((number - 1) / 7);
+    const perfectClass = [1, 4, 5].includes(simple);
+    const majorBase = { 1: 0, 2: 2, 3: 4, 4: 5, 5: 7, 6: 9, 7: 11 }[simple] + octaves * 12;
+    let adjustment;
+    if (perfectClass) {
+      if (quality === 'P') adjustment = 0;
+      else if (quality === 'A') adjustment = 1;
+      else if (quality === 'd') adjustment = -1;
+      else throw new Error(`${quality}${number} is not a valid perfect-class interval.`);
+    } else {
+      if (quality === 'M') adjustment = 0;
+      else if (quality === 'm') adjustment = -1;
+      else if (quality === 'A') adjustment = 1;
+      else if (quality === 'd') adjustment = -2;
+      else throw new Error(`${quality}${number} is not a valid major-class interval.`);
+    }
+    return majorBase + adjustment;
+  }
+
+  function buildTriad(rootPc, quality = 'major') {
+    const intervals = { major: [0, 4, 7], minor: [0, 3, 7], diminished: [0, 3, 6], augmented: [0, 4, 8] }[quality] || [0, 4, 7];
+    return intervals.map(interval => mod(rootPc + interval, 12));
+  }
+
+  function diatonicTriad(degree, key = 'C', mode) {
+    const scale = scaleForKey(key, mode);
+    const index = mod(degree - 1, 7);
+    return [scale[index], scale[(index + 2) % 7], scale[(index + 4) % 7]];
+  }
+
+  function nearestPitchClass(pc, target, min, max) {
+    let best = null;
+    let distance = Infinity;
+    for (let midi = min; midi <= max; midi += 1) {
+      if (mod(midi, 12) !== mod(pc, 12)) continue;
+      const current = Math.abs(midi - target);
+      if (current < distance) { best = midi; distance = current; }
+    }
+    return best == null ? clamp(target, min, max) : best;
+  }
+
+  function durationName(beats) {
+    const map = new Map([[8, 'breve'], [6, 'dotted whole'], [4, 'whole'], [3, 'dotted half'], [2, 'half'], [1.5, 'dotted quarter'], [1, 'quarter'], [.75, 'dotted eighth'], [.5, 'eighth'], [.375, 'dotted sixteenth'], [.25, 'sixteenth'], [.125, 'thirty-second'], [.0625, 'sixty-fourth']]);
+    return map.get(Number(beats)) || `${beats}-beat note`;
+  }
+
+  function durationDots(beats) { return [6, 3, 1.5, .75, .375].includes(Number(beats)) ? 1 : 0; }
+  function baseDuration(beats) { return durationDots(beats) ? Number(beats) / 1.5 : Number(beats); }
+  function flagCount(beats) {
+    const base = baseDuration(beats);
+    if (base >= 1) return 0;
+    return clamp(Math.round(Math.log2(1 / base)), 1, 4);
+  }
+
+  function noteTypeFromBeats(beats) {
+    const base = baseDuration(beats);
+    if (base >= 8) return 'breve';
+    if (base >= 4) return 'whole';
+    if (base >= 2) return 'half';
+    if (base >= 1) return 'quarter';
+    if (base >= .5) return 'eighth';
+    if 4base >= .25) return '16th';
+    if (base >= .125) return '32nd';
+    return '64th';
+  }
+
+  function frequencyForMidi(midi) { return 440 * Math.pow(2, (Number(midi) - 69) / 12); }
+
+  return {
+    PITCH_CLASSES_SHARP, PITCH_CLASSES_FLAT, LETTERS, KEY_SIGNATURES, MAJOR_SCALE, NATURAL_MINOR_SCALE,
+    SHARP_ORDER, FLAT_ORDER, clamp, mod, parsePitch, accidentalText, pitchToMidi, midiToPitch,
+    keyRoot, keyRootPc, isMinorKey, keySignatureCount, keySignatureAlteration, scaleForKey,
+    pitchToDiatonicIndex, diatonicIndexToNatural, pitchFromDiatonicIndex, spellMidiForKey, transposeKey,
+    clefBottomLineIndex, staffStepForPitch, pitchForStaffStep, displayMidiForPart, soundingMidiFromDisplay,
+    writtenPitchForEvent, degreeForPitch, degreeForMidi, tonicReferenceMidi, tonicSolfaForPitch,
+    tonicSolfaForMidi, validatePitchIdentity, intervalSemitones, buildTriad, diatonicTriad, nearestPitchClass,
+    durationName, durationDots, baseDuration, flagCount, noteTypeFromBeats, frequencyForMidi
+  };
+});
